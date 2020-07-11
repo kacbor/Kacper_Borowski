3fa84b6f73ff394dccf21b7626534085d6b1e767
kacbor <borowski.kacper@gmail.com>
2020-07-11 18:12:29 +0200

    po zad2b/lab8

---
+++ game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kacper_Borowski
+{
+   abstract class Game
+    {
+        protected Player playerOne, playerTwo;
+        protected Dictionary<string, string> inputTable;
+        public GamesRecord gamesRecord;
+        public abstract void Play();
+
+    }
+}
