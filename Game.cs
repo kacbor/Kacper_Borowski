51626d063b3fedc8eb20c1c38088f90bdc494003
kacbor <borowski.kacper@gmail.com>
2020-07-11 20:28:11 +0200

    po zad 1b/lab9

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kacper_Borowski
+{
+   abstract class Game
+    {
+        protected Player playerOne, playerTwo;
+        protected Dictionary<string, string> inputTable;
+        public GamesRecord gamesRecord;
+        public abstract void Play();
+
+        private static string gameName;
+        private static string gameRules;
+  
+  public string GameName { get => gameName; set => gameName = value; }
+        public string GameRules { get => gameRules; set => gameRules = value; }
+
+
+    }
+}
---
+++ RecordRPS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kacper_Borowski
+{
+    class RecordRPS : IRecord
+    {
+        string playerOneChoice;
+        string playerTwoChoice;
+        string result;
+
+        public RecordRPS(string playerOneChoice, string playerTwoChoice, string result)
+        {
+            this.playerOneChoice = playerOneChoice;
+            this.playerTwoChoice = playerTwoChoice;
+            this.result = result;
+        }
+
+        override public string ToString()
+        {
+            return string.Format("{0,20} : {1,-20}", playerOneChoice + " vs " + playerTwoChoice, result);
+        }
+
+    }
+}
